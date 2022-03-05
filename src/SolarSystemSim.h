06f6d414afdc59ab4e5e3b140c35d8c263e2d6ea
vishnu <vishnu14july@gmail.com>
2022-03-05 17:28:27 +0530

    inital setup

---
+++ src/SolarSystemSim.h
@@ -0,0 +1,13 @@
+﻿// SolarSystemSim.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+// TODO: Reference additional headers your program requires here.
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+
