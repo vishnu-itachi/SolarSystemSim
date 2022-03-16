975939412b9ef82dc577dcdb388fdac1d655193f
Lakkam Sai Krishna Reddy <lakkam.saikrishnareddy@gmail.com>
2022-03-16 19:44:12 +0530

    separated the simulation from rendering.

--- src/SolarSystemSim.cpp
+++ src/SolarSystemSim.cpp
@@ -116,17 +116,26 @@ int main()
         glEnableVertexAttribArray(ATTRIB_VALUE);
         glVertexAttribPointer(ATTRIB_VALUE, 2, GL_FLOAT, GL_FALSE, 16, BUFFER_OFFSET(8));
 
-        while (!glfwWindowShouldClose(window))
+        std::vector<std::vector<float>> simulated_positions;
+        auto num_steps = 1000;
+        for (unsigned int i = 0; i < num_steps; i++)
+        {
+            std::vector<float> vertices;
+            add_point_vertices(sim.bodies(), vertices);
+            simulated_positions.push_back(vertices);
+            sim.forward();
+        }
+
+        int i = 0;
+        while (!glfwWindowShouldClose(window) && i < simulated_positions.size())
         {
             CheckedGLCall(glClear(GL_COLOR_BUFFER_BIT));
 
-            std::vector<float> vertices;
-            add_point_vertices(sim.bodies(), vertices);
+            auto vertices = simulated_positions[i++];
             vb.send_data(vertices.data(), vertices.size() * 4);
 
             CheckedGLCall(glDrawArrays(GL_TRIANGLES, 0, vertices.size()));
 
-            sim.forward();
             sim.wait();
             
             glfwSwapBuffers(window);
